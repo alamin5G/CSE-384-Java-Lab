b54d2ddf2d1fe040cba7aeeed04662e88bd153a2
alamin5g <alaminrakib4864@gmail.com>
2024-11-03 08:32:59 +0600

    added jCalander to the Spending Tracker and removed the Bug

---
+++ SpendingTracker/src/db/DbConnect.java
@@ -0,0 +1,17 @@
+package db;
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class DbConnect {
+    public static Connection c;
+    public static Statement st;
+    static{
+        try{
+            c=DriverManager.getConnection(
+      "jdbc:mysql://localhost:3306/spnedingTracker"+"?useSSL=false",
+                 "root", "252646");
+            st=c.createStatement();
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+}
