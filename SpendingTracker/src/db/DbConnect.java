868ac90b9e9722e12969da807c3a29b4bbdfa0ac
Md Al Amin <21303134@iubat.edu>
2024-11-03 17:33:10 +0600

    spending tracker added

---
+++ SpendingTracker/src/db/DbConnect.java
@@ -0,0 +1,17 @@
+package db;
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class DbConnect {
+    public static Connection c;
+    public static Statement st;
+    static{
+        try{
+            c=DriverManager.getConnection(
+      "jdbc:mysql://localhost:3306/spendingTracker"+"?useSSL=false",
+                 "root", "252646");
+            st=c.createStatement();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+}
